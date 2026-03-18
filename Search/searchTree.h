04e2db624ba9762dac7d31070956b0d209317aba
Alexa <Alexander.tuero1205@gmail.com>
2026-03-18 11:55:05 -0400

    completed depth_search functionality

--- Search/searchTree.h
+++ Search/searchTree.h
@@ -2,32 +2,30 @@
 #include <string>
 #include <vector>
 #include <set>
+#include <unordered_set>
 #include <iostream>
 
 struct searchTree{
 private:
-    //article node holds info
-    struct anode{
-        int id;
-        std::vector<int> idpaths;
 
-        anode(int _id, std::vector<int> _idpaths): id(_id), idpaths(_idpaths){};
-    };
 
-    anode* root;
+    //kept global for depth search
+    int shortestpath = INT_MAX;
 
-    //set of all visited nodes
-    std::set<int> visited;
+    //kept global for all searches
+    std::vector<std::vector<int>> paths;
+
+
+    //possibily future function that gets data
+    //for now it is a placeholder
+    std::vector<int> get_children(int parent) {
+        std::vector<int> returnVec;
+        return returnVec;
+    }
 
 public:
-    searchTree(int _id, std::vector<int> _idpaths){
-        root->id = _id;
-        root->idpaths = _idpaths;
-    };
-    //destructor will depend on how the rest pans out
-    //if we make the visited set store anodes rather than ids we could use it to delete them all after
-    //but turning every id into an anode may be computationally intensive
-    ~searchTree() {}
+    searchTree()= default;
+    ~searchTree() = default;
 
     //---------------------------------------------------------------------------------------------
     //SEARCHES
@@ -35,17 +33,56 @@ public:
     //maybe helper functions for recursion?
     //---------------------------------------------------------------------------------------------
 
-    //depth first search (pre-order)
-    std::vector<std::vector<int>> depth_search(int target){
-        std::vector<std::vector<int>> returnVec;
-        int shortestpath = INT_MAX;
-        return returnVec;
-    };
+    //depth first search
+    std::vector<std::vector<int>> depth_search(int start, int target){
+        //containers to be reused
+        std::vector<int> path;
+        std::unordered_set<int> localvisited;
+
+        //first function call
+        depth_helper(path, localvisited, 0, target, start);
+
+        //clean up and return
+        std::vector<std::vector<int>> pathscopy = paths;
+        shortestpath = INT_MAX;
+        paths.clear();
+        return pathscopy;
+    }
+    void depth_helper(std::vector<int>& v, std::unordered_set<int>& vis, int curdepth, int target, int current) {
+        //tap out checks
+        if (curdepth > shortestpath) { return; }
+        if (vis.count(current)) { return; } //if count != 0
+
+        //node visit is allowed
+        v.push_back(current);
+
+        //if found target
+        if (current == target) {
+            //new shortest path
+            if (curdepth < shortestpath) {
+                shortestpath = curdepth;
+                paths.clear();
+            }
+            paths.push_back(v);
+            v.pop_back(); //remove target from list and keep searching
+            return;
+        }
+
+        //recurse
+        vis.insert(current);
+        std::vector<int> children = get_children(current);
+        for (int child: children) {
+            depth_helper(v, vis, curdepth + 1, target, child);
+        }
+        //wrap up recurse
+        vis.erase(current);
+        v.pop_back();
+    }
 
     //breadth first search
     std::vector<std::vector<int>> breadth_search(int target){
         std::vector<std::vector<int>> returnVec;
         std::vector<int> curlevel;
         return returnVec;
-    };
+    }
 };
